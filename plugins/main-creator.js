530aa038f38b6a9a0c3a5a3d0eea7bb98044146e
Aqua200 <chinquepapa@gmail.com>
2024-11-30 18:14:49 -0800

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱;;\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Owner Zamus⁩👑;;\nFN:𝙾𝚠𝚗𝚎𝚛 𝚉𝚊𝚖𝚞𝚜⁩👑\nORG:𝙾𝚠𝚗𝚎𝚛 𝚉𝚊𝚖𝚞𝚜⁩👑\nTITLE:\nitem1.TEL;waid=573503418427:573503418427\nitem1.X-ABLabel:𝙾𝚠𝚗𝚎𝚛 𝚉𝚊𝚖𝚞𝚜⁩👑\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝙾𝚠𝚗𝚎𝚛 𝚉𝚊𝚖𝚞𝚜⁩👑\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
