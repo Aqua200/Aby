b474a7b33d2fb8559d70982ef3e5397f3bd3c64f
Aqua200 <chinquepapa@gmail.com>
2024-11-30 18:50:30 -0800

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -18,7 +18,7 @@ let img = catalogo
 					mediaUrl: null,
 					description: null,
 					previewType: "PHOTO",
-					thumbnailUrl: '',
+					thumbnailUrl: 'https://qu.ax/GwMFx.jpg',
 		           sourceUrl: canal,
 		           mediaType: 1,
                    renderLargerThumbnail: false
